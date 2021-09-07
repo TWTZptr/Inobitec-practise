13ae7abaa528249ca579f6dbd00f3efa0d8baea7
TWTZ <twtzzz@yandex.ru>
2021-09-07 20:31:23 +0300

    dotenv added

--- db.js
+++ db.js
@@ -1,7 +1,13 @@
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
-module.exports = new Sequelize('network', 'postgres', 'admin', {
-    port: '5432',
+const dbName = process.env.DB_NAME;
+const dbPassword = process.env.DB_PASSWORD;
+const dbPort = process.env.DB_PORT;
+const dbLogin = process.env.DB_LOGIN;
+
+module.exports = new Sequelize(dbName, dbLogin, dbPassword, {
+    port: dbPort,
     logging: false,
     dialect: 'postgres',
     host: 'localhost',
